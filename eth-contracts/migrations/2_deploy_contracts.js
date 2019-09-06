db2f4105638b1a5f54ea640ed3ff191114cf2c4e
Faried Nawaz <faried@gmail.com>
2019-09-06 05:09:04 +0500

    deploy script: need SquareVerifier.address for SolnSquareVerifier.

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@
 const SquareVerifier = artifacts.require("./verifier.sol");
 const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-module.exports = function(deployer) {
-    deployer.deploy(SquareVerifier);
-    deployer.deploy(SolnSquareVerifier);
+module.exports = async function(deployer) {
+    await deployer.deploy(SquareVerifier);
+    await deployer.deploy(SolnSquareVerifier, SquareVerifier.address);
 };
